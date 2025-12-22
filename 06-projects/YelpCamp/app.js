5afd0a798cc94ddd6946019436384021f28eb227
Olubodun Adeola <olubodunadeola2000@gmail.com>
2025-12-22 02:04:20 +0100

    Connect MongoDB to mongoose and express

---
+++ 06-projects/YelpCamp/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.log.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log('Database connected');
+})
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/campground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyaed', description: 'cheap camping' });
+    await camp.save();
+    res.send(camp);
+})
+
+
+app.listen('3000', () => {
+    console.log("LISTENING ON PORT 3000!");
+})
