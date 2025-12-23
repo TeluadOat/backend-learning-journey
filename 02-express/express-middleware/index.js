f3cb6c6aa3f39a5aa3c4931a610aae64de630379
Olubodun Adeola <olubodunadeola2000@gmail.com>
2025-12-23 23:59:18 +0100

    continue to learn express middleware

--- 02-express/express-middleware/index.js
+++ 02-express/express-middleware/index.js
@@ -5,22 +5,50 @@ const app = express();
 
 app.use(morgan('tiny'));
 
-app.use((req, res, next) => {
-    console.log('This is my first middleware!');
-    return next();
-});
+// app.use((req, res, next) => {
+//     console.log('This is my first middleware!');
+//     return next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log('This is my second middleware!');
+//     return next();
+// });
+
+const verifyPassword = (req, res, next) => {
+    const { password } = req.query;
+    if (password === "teluadoat") {
+        return next();
+    }
+    res.send('You need a password!')
+};
 
 app.use((req, res, next) => {
-    console.log('This is my second middleware!');
-    return next();
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
 });
+
+app.use('/dogs', (req, res, next) => {
+    console.log('I love dogs!!');
+    next();
+})
+
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('MY SECRET IS: I am really working hard to be a great software engineer.');
+})
 
 app.get('/', (req, res) => {
+    console.log('REQUEST TIME: ', new Date(req.requestTime));
     res.send('Home Page');
 });
 
 app.get('/dogs', (req, res) => {
     res.send('woof woof');
+});
+
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND');
 })
 
 app.listen(3000, () => {
