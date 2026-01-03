62d47b3a735084600f528620ee2b313e53023e76
Olubodun Adeola <olubodunadeola2000@gmail.com>
2026-01-03 23:48:21 +0100

    feat(campgrounds): add deletion middleware to delete reviews associated with campground on campground deletion

--- 06-projects/YelpCamp/models/campground.js
+++ 06-projects/YelpCamp/models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const campgroundSchema = new Schema({
@@ -10,9 +11,19 @@ const campgroundSchema = new Schema({
     reviews: [
         {
             type: Schema.Types.ObjectId,
-            ref: 'Review'
+            ref: 'Review',
         }
     ]
 });
+
+campgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews,
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', campgroundSchema);
