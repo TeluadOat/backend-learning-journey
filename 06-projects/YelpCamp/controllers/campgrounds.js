9da4e807307b73ab7c3b8c28c6ce9186b4faa52e
Olubodun Adeola <olubodunadeola2000@gmail.com>
2026-01-09 00:57:35 +0100

    refactor(campgrounds): separate controller logic from app.js

---
+++ 06-projects/YelpCamp/controllers/campgrounds.js
@@ -0,0 +1,54 @@
+const Campground = require('../models/campground');
+
+const index = async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+};
+
+const newCampgroundForm = (req, res) => {
+    res.render('campgrounds/new');
+};
+
+const showCampground = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id).populate('reviews');
+    res.render('campgrounds/show', { campground })
+};
+
+
+const createCampground = async (req, res) => {
+    async (req, res) => {
+        // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`);
+    };
+};
+
+const editCampgroundForm = async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+};
+
+const updateCampground = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
+};
+
+const deleteCampground = async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+};
+
+module.exports = {
+    index,
+    newCampgroundForm,
+    showCampground,
+    createCampground,
+    editCampgroundForm,
+    updateCampground,
+    deleteCampground
+};
