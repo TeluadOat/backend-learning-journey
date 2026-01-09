404ad77fef25c5a7529ebb8e11bcf334821f695e
Olubodun Adeola <olubodunadeola2000@gmail.com>
2026-01-09 01:00:32 +0100

    refactor(app): move validation logic into reusable middleware

---
+++ 06-projects/YelpCamp/middleware/validate.js
@@ -0,0 +1,19 @@
+const ExpressError = require('../utils/ExpressError');
+
+const validate = (schema, property = "body") => {
+	return (req, res, next) => {
+		const { value, error } = schema.validate(req[body], {
+			abortEarly: false,
+			stripUnknown: true
+		});
+
+		if (error) {
+			const msg = error.details.map(el => el.message).join(',');
+			throw new ExpressError(msg, 400);
+		}
+		req[property] = value;
+		next();
+	};
+};
+
+module.exports = validate;
