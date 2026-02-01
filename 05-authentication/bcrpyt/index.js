c256e5f083bfdf9b4e492c3fa8a7308756a8d6e9
Olubodun Adeola <olubodunadeola2000@gmail.com>
2026-02-01 23:09:37 +0100

    minor change

--- 05-authentication/bcrpyt/index.js
+++ 05-authentication/bcrpyt/index.js
@@ -18,25 +18,3 @@ const login = async (password, hashedPassword) => {
     await login(password, hashedPassword);
 })();
 
-
-
-
-// const bcrypt = require("bcrypt");
-
-// const password = "myPlainPassword";
-
-// const hashPassword = async (password) => {
-//   const hashed = await bcrypt.hash(password, 10);
-//   return hashed;
-// };
-
-// const login = async (password, hashPassword) => {
-//   const isMatch = await bcrypt.compare(password, hashPassword);
-//   if (isMatch) console.log("Authenticated");
-//   else console.log("Password not matched");
-// };
-
-// (async () => {
-//   const hashedPassword = await hashPassword(password);
-//   await login(password, hashedPassword);
-// })();
