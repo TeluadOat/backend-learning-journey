65687006f0e8990774668fd3f3a2938ae3fe37f7
Olubodun Adeola <olubodunadeola2000@gmail.com>
2026-02-06 13:50:28 +0100

    reseed campground collection to include a single author for all campgrounds

--- 06-projects/YelpCamp/seeds/index.js
+++ 06-projects/YelpCamp/seeds/index.js
@@ -20,6 +20,7 @@ const seedDb = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '698113edad2a7cffe3d16800',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)}, ${sample(places)}`,
             image: 'https://picsum.photos/400?random=${Math.random()}',
