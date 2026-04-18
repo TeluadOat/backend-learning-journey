0b3780b0d08e94d516a5262d6a23fd69821eeb0f
Olubodun Adeola <olubodunadeola2000@gmail.com>
2026-04-18 05:06:36 +0100

    Install multer, multer-storage-cloudinary, cloudinary
     -configure multer

--- 06-projects/YelpCamp/app.js
+++ 06-projects/YelpCamp/app.js
@@ -9,6 +9,10 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const User = require('./models/user');
 
 const usersRoutes = require('./routes/users')
