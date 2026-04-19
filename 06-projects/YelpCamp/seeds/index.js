6f5790548ef6a9ab624260a5c31c4c84bf6c4f06
Olubodun Adeola <olubodunadeola2000@gmail.com>
2026-04-19 11:21:07 +0100

    reflect image property change in schema

--- 06-projects/YelpCamp/seeds/index.js
+++ 06-projects/YelpCamp/seeds/index.js
@@ -23,9 +23,18 @@ const seedDb = async () => {
             author: '698113edad2a7cffe3d16800',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)}, ${sample(places)}`,
-            image: 'https://picsum.photos/400?random=${Math.random()}',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dmckclwvx/image/upload/v1776582854/YelpCamp/ylu8ykpk8rpu3nshqgfc.png',
+                    fileName: 'YelpCamp/ylu8ykpk8rpu3nshqgfc',
+                },
+                {
+                    url: 'https://res.cloudinary.com/dmckclwvx/image/upload/v1776582855/YelpCamp/pplvum3liw7i5mxj16bc.png',
+                    fileName: 'YelpCamp/pplvum3liw7i5mxj16bc',
+                }
+            ],
         });
         await camp.save();
     }
