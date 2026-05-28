d967e5d661a1349c21c004b2357b917758e7e87b
Olubodun Adeola <olubodunadeola2000@gmail.com>
2026-05-28 18:28:10 +0100

    fix(map): enlarge unclustered point hit target so map popups open reliably

--- 06-projects/YelpCamp/seeds/index.js
+++ 06-projects/YelpCamp/seeds/index.js
@@ -12,7 +12,7 @@ db.once("open", () => {
     console.log('Database connected');
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDb = async () => {
     await Campground.deleteMany({});
@@ -20,25 +20,45 @@ const seedDb = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '698113edad2a7cffe3d16800',
+            author: '6a110952add3f340ac27c15f',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)}, ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
             images: [
                 {
-                    url: 'https://res.cloudinary.com/dmckclwvx/image/upload/v1776582854/YelpCamp/ylu8ykpk8rpu3nshqgfc.png',
-                    fileName: 'YelpCamp/ylu8ykpk8rpu3nshqgfc',
-                },
-                {
-                    url: 'https://res.cloudinary.com/dmckclwvx/image/upload/v1776582855/YelpCamp/pplvum3liw7i5mxj16bc.png',
-                    fileName: 'YelpCamp/pplvum3liw7i5mxj16bc',
+                    url: 'https://res.cloudinary.com/dmckclwvx/image/upload/v1779501180/YelpCamp/dcrbpizyjtvi6svwq7lx.png',
+                    fileName: 'YelpCamp/dcrbpizyjtvi6svwq7lx',
                 }
             ],
         });
         await camp.save();
     }
+
+
 }
-seedDb().then(() => {
-    db.close();
-})
+
+const run = async () => {
+    try {
+        await seedDb();
+
+        console.log('Database seeded successfully!');
+        console.log('Closing database connection...');
+
+        await mongoose.connection.close();
+
+        console.log('Database connection closed.');
+    } catch (err) {
+        console.error('Error seeding database:', err);
+        await mongoose.connection.close();
+    }
+};
+
+run();
